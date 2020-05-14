878d6f30ffadaef117301513977487487ffd995e
msx2mac <tsuyoshi.redding@gmail.com>
2020-05-14 09:19:02 +0900

    change super key

--- keyboards/lily58/keymaps/kokemaru/keymap.c
+++ keyboards/lily58/keymaps/kokemaru/keymap.c
@@ -37,9 +37,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * CTL = Control
 */
 
-#define X_LOW LT(_LOWER, KC_LGUI)    // Tap: Eng, Hold: Lower
-#define X_RAI LT(_RAISE, KC_RGUI)    // Tap: Jpn, Hold: Raise
-#define X_RAIBS LT(_RAISE, KC_BSPC)  // Tap: Bspc, Hold: Raise
+// #define X_LOW LT(_LOWER, KC_LGUI)    // Tap: Eng, Hold: Lower
+// #define X_RAI LT(_RAISE, KC_RGUI)    // Tap: Jpn, Hold: Raise
+// #define X_RAIBS LT(_RAISE, KC_BSPC)  // Tap: Bspc, Hold: Raise
+#define LOW TT(_LOWER)
+#define RAI TT(_RAISE)
 
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -52,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  \   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI | LOWER| /Space  /       \Enter \  |RAISE | RGUI | ESC  |
- *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   |      |      | LOCK |/       /         \      \ |LOCK  |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_QWERTY] = LAYOUT( \
@@ -60,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL, \
   KC_LCTRL, KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,    KC_BSPC, KC_DEL,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, \
-                            KC_LALT, LOWER,   KC_LGUI, KC_SPC,  KC_ENT,   RAISE,   KC_RGUI, KC_ESC \
+                            KC_LALT, LOW,     KC_LGUI, KC_SPC,  KC_ENT,   RAI,     KC_RGUI, KC_ESC \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -68,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |  F11 |  F12 |PrintS|SCRLK |Pause |                    |      |   7  |   8  |   9  |  +   |  *   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LCTRL |   (  |   )  |      |      |      |-------.    ,-------|      |   4  |   5  |   6  |  -   |  /   |
+ * |LCTRL |   (  |   )  |      |      |      |-------.    ,-------| .    |   4  |   5  |   6  |  -   |  /   |
  * |------+------+------+------+------+------|BackSP |    | DEL   |------+------+------+------+------+------|
  * |LShift|   [  |   ]  |   {  |   }  |      |-------|    |-------|   0  |   1  |   2  |   3  |Enter |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -78,9 +80,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT( \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX, \
-  _______, KC_F11,  KC_F12,  KC_PSCR, KC_SLCK, KC_PAUS,                   XXXXXXX, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_PAST, \
-  _______, KC_LPRN, KC_RPRN, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_P4,   KC_P5,   KC_P6,   KC_PMNS, KC_PSLS, \
-  _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, XXXXXXX, _______, _______, KC_0,    KC_P1,   KC_P2,   KC_P3,   KC_PENT, XXXXXXX, \
+  _______, KC_F11,  KC_F12,  KC_PSCR, KC_SLCK, KC_PAUS,                   XXXXXXX, KC_7,    KC_8,    KC_9,    KC_PLUS, KC_ASTR, \
+  _______, KC_LPRN, KC_RPRN, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_DOT,  KC_4,    KC_5,    KC_6,    KC_MINS, KC_SLSH, \
+  _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, XXXXXXX, _______, _______, KC_0,    KC_1,    KC_2,    KC_3,    KC_ENT,  XXXXXXX, \
                              _______, _______, _______, _______, _______,  _______, _______, _______\
 ),
 /* RAISE
@@ -108,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |                    |      | MUTE | PUS= |      |      |      |
+ * |      |      |      |      |      |      |                    |      | MUTE |MEDIA |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------.    ,-------|      | VOL+ | TRA+ | BRI+ |      |      |
  * |------+------+------+------+------+------|BackSP |    | DEL    |------+------+------+------+------+------|
@@ -208,41 +210,5 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     // set_timelog();
   }
 
-  switch (keycode) {
-    case QWERTY:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_QWERTY);
-      }
-      return false;
-      break;
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case ADJUST:
-        if (record->event.pressed) {
-          layer_on(_ADJUST);
-        } else {
-          layer_off(_ADJUST);
-        }
-        return false;
-        break;
-  }
   return true;
 }
